597d96ab38957b4934cd331c77df6631f597e0ed
Lev <95850699+narcissusTheFlower@users.noreply.github.com>
2024-04-21 23:00:34 +0300

    Update fresh_popos_installation.sh

--- fresh_popos_installation.sh
+++ fresh_popos_installation.sh
@@ -11,7 +11,6 @@ echo "options snd-hda-intel power_save=0\npower_save_controller=N" >> /etc/modpr
 echo "~~~INSTALLING PACKAGES~~~"
 apt install discord, boxes, netstat-tools, sensors, neofetch, flameshot, git, obs-studio, postman, okular, postgresql, openjdk-17-jdk, openjdk-17-doc, openjdk-17-source -y
 
-echo "user ALL=(ALL) NOPASSWD: ALL" >> 
 #confgiure harddrive (chmod, chown)
 #disable sudo with `sudo visudo` and then at the end of the file add next line:
 #"user" ALL=(ALL) NOPASSWD: ALL
