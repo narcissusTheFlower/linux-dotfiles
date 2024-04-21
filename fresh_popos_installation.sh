4531a2536ba465e736674c336a0370691457a6cb
Lev <95850699+narcissusTheFlower@users.noreply.github.com>
2024-04-21 14:46:46 +0300

    Add files via upload

---
+++ fresh_popos_installation.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+sudo su; 
+apt update; 
+apt upgrade -y
+
+echo "~~~CONFIGUREING NEOFETCH AND FIXING SOUND~~~"
+echo "neofetch\n" >> ~/.bashrc;
+echo "options snd-hda-intel power_save=0\npower_save_controller=N" >> /etc/modprobe.d/alsa-base.conf
+
+echo "~~~INSTALLING PACKAGES~~~"
+apt install discord, boxes, netstat-tools, sensors, neofetch, flameshot, git, obs-studio, postman, okular, postgresql, openjdk-17-jdk, openjdk-17-doc, openjdk-17-source -y
+
+
+#confgiure harddrive (chmod, chown)
+
+
+
+
