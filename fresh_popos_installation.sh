0be4b668bb8f79829a8f611e3c7907c1511c0ebe
Lev <95850699+narcissusTheFlower@users.noreply.github.com>
2024-04-22 22:20:55 +0300

    Update fresh_popos_installation.sh

--- fresh_popos_installation.sh
+++ fresh_popos_installation.sh
@@ -9,10 +9,12 @@ echo "neofetch\n" >> ~/.bashrc;
 echo "options snd-hda-intel power_save=0\npower_save_controller=N" >> /etc/modprobe.d/alsa-base.conf
 
 echo "~~~INSTALLING PACKAGES~~~"
-apt install discord, boxes, netstat-tools, sensors, neofetch, flameshot, git, obs-studio, postman, okular, postgresql, openjdk-17-jdk, openjdk-17-doc, openjdk-17-source -y
-
+apt install discord, boxes, netstat-tools, sensors, neofetch, flameshot, git, obs-studio, postman, okular, postgresql, openjdk-17-jdk, openjdk-17-doc, openjdk-17-source -y;
+sleep 2;
+reboot;
+#wget https://s3.amazonaws.com/outline-releases/client/linux/stable/Outline-Client.AppImage
 #confgiure harddrive (chmod, chown)
 #disable sudo with `sudo visudo` and then at the end of the file add next line:
 #"user" ALL=(ALL) NOPASSWD: ALL
-
+#configure gnome extentions through the website first and then import settings from "dash-to-panel-gnome-extentions" in this repo
 
