5adaaebdc88a53e225662eefbca8a8a28c979942
Lev <95850699+narcissusTheFlower@users.noreply.github.com>
2024-04-23 18:39:49 +0300

    Update fresh_popos_installation.sh

--- fresh_popos_installation.sh
+++ fresh_popos_installation.sh
@@ -21,4 +21,5 @@ reboot;
 #disable sudo with `sudo visudo` and then at the end of the file add next line:
 #"user" ALL=(ALL) NOPASSWD: ALL
 #configure gnome extentions through the website first and then import settings from "dash-to-panel-gnome-extentions" in this repo
+#dash to panel extention https://extensions.gnome.org/extension/1160/dash-to-panel/
 
