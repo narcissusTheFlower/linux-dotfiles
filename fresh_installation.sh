9459694e057ff7e780755ea331151c3736d63e99
Lev <95850699+narcissusTheFlower@users.noreply.github.com>
2024-11-14 13:43:07 +0300

    Update fresh_installation.sh

--- fresh_installation.sh
+++ fresh_installation.sh
@@ -1,26 +1,26 @@
-#/bin/bash
-
-sudo su; 
-add-apt-repository ppa:jonaski/strawberry
-apt update; 
-apt upgrade -y
-
-echo "~~~CONFIGUREING NEOFETCH AND FIXING SOUND~~~"
-echo "neofetch" >> ~/.bashrc;
-#if /etc/modprobe.d/alsa-base.conf does not exist create it
-
+#For all distros
 #Go into `alsamixer` change auto-mute to disabled
-echo "options snd-hda-intel power_save=0 power_save_controller=N" >> /etc/modprobe.d/alsa-base.conf
-#idk what it does but it might fix sound "modprobe -r snd-hda-intel && modprobe snd-hda-intel". It will always somehow work after a couple of reboots
+#The issue where the sound card will go into power-save mode will fix itself after some alsactl restore and reboots ig?? it has always been random for me but always fixed itself in the end :v
+echo "options snd-hda-intel power_save=0 power_save_controller=N" >> /etc/modprobe.d/alsa-base.conf #if /etc/modprobe.d/alsa-base.conf does not exist create it
+modprobe -r snd-hda-intel && modprobe snd-hda-intel
 
+#For OpeneSuse Leap
+sudo zypper addrepo -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_$releasever/' packman
+sudo zypper ref
+sudo zypper update
+sudo zypper dup --from pacman --allow-vendor-change
+
+sudo zypper in gnome-boxes git traceroute
+
+git config --global user.name "narcissusTheFlower"
+git config --global user.email narcissustheflower2047@gmail.com
+
+
+#For apt based distros
 echo "~~~INSTALLING PACKAGES~~~"
 apt install alsa-tools discord boxes sensors neofetch flameshot obs-studio okular postgresql openjdk-17-jdk openjdk-17-doc openjdk-17-source strawberry traceroute -y;
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/obsidian_1.5.12_amd64.deb;
-sleep 2;
-reboot;
 
-#in alsa-mixer change automute to "disabled"
-#the issue where the sound card will go into power-save mode will fix itself after some alsactl restore and reboots ig?? it has always been random for me but always fixed itself in the end :v
 #confgiure harddrive (chmod, chown)
 #disable sudo with `sudo visudo` and then at the end of the file add next line:
 #"user" ALL=(ALL) NOPASSWD: ALL
@@ -30,8 +30,6 @@ reboot;
 #remove blur https://extensions.gnome.org/extension/2935/control-blur-effect-on-lock-screen/
 #removable media icon https://extensions.gnome.org/extension/7/removable-drive-menu/
 #sound extention https://extensions.gnome.org/extension/906/sound-output-device-chooser/
-git config --global user.name "narcissusTheFlower"
-git config --global user.email narcissus_mail@mail.ru
 
 #Screenshot
 #Remove default gnome keyboard shortcut in Settings -> Keyboard -> Keyboard Shortcuts. Find "Interactive screenshot" option and remove the key
